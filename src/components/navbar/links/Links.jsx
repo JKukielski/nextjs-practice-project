7e01cb9300fe244e6542e6171dd6b6be02e9641e
JKukielski <jankukielski1@gmail.com>
2024-03-08 21:03:00 +0100

    Structure, styling and session/admin placeholders

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,19 +1,30 @@
-import Link from 'next/link';
-
+import styles from './links.module.css';
+import NavLink from './navLink/navLink';
 const Links = () => {
   const links = [
     { title: 'Homepage', path: '/' },
     { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/' },
+    { title: 'Contact', path: '/Contact' },
     { title: 'Blog', path: '/blog' },
   ];
+
+  //temp placeholders
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+        <NavLink item={link} key={link.title} />
+      ))}{' '}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: 'Login', path: '/login' }} />
+      )}
     </div>
   );
 };
