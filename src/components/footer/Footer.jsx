1561de8159a8507499f9d9e524e413f858cd7a78
JKukielski <jankukielski1@gmail.com>
2024-03-09 15:46:14 +0100

    Footer UI

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.text}>
+        A creative thoughts agency. All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
