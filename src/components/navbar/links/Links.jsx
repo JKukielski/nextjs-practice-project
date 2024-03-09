e1ff684476b34f504047dc8ea25342c0cd8b8aed
JKukielski <jankukielski1@gmail.com>
2024-03-09 12:32:03 +0100

    Navbar responsiveness

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,29 +1,49 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
+
+const links = [
+  { title: 'Homepage', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/Contact' },
+  { title: 'Blog', path: '/blog' },
+];
 const Links = () => {
-  const links = [
-    { title: 'Homepage', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/Contact' },
-    { title: 'Blog', path: '/blog' },
-  ];
+  const [open, setOpen] = useState(false);
 
   //temp placeholders
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}{' '}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: 'Login', path: '/login' }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}{' '}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: 'Login', path: '/login' }} />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
