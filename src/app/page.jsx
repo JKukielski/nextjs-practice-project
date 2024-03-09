ae4639f9e1fa784a7c9362056621dda60f16ec92
JKukielski <jankukielski1@gmail.com>
2024-03-09 12:47:10 +0100

    UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import Image from 'next/image';
+import styles from './home.module.css';
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+          accusamus a officia in quia culpa accusantium cum quidem tempore
+          dolorem.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
