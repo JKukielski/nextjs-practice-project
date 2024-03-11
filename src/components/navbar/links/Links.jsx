3c7a7827f948bbd1f7ee47a7e7069e881b2fad1a
JKukielski <jankukielski1@gmail.com>
2024-03-11 19:52:56 +0100

    Added burger icon for mobile menu

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
+import Image from 'next/image';
 
 const links = [
   { title: 'Homepage', path: '/' },
@@ -32,12 +33,15 @@ const Links = () => {
           <NavLink item={{ title: 'Login', path: '/login' }} />
         )}
       </div>
-      <button
-        className={styles.menuButton}
+
+      <Image
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prev) => !prev)}
-      >
-        Menu
-      </button>
+        className={styles.menuButton}
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
