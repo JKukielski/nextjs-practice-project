82ac85f0dcb934f3f6e4b6335a885023463e6689
JKukielski <jankukielski1@gmail.com>
2024-03-11 19:53:22 +0100

    Added placeholder image link

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -7,7 +7,12 @@ const PostCard = () => {
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/post.png" alt="" fill className={styles.img} />
+          <Image
+            src="https://images.unsplash.com/photo-1710088912041-34d1767d376a?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            className={styles.img}
+          />
         </div>
         <span className={styles.date}>01.01.2024</span>
       </div>
