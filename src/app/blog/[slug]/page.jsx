2a6a0afa75294aced85743850642fdb0ae0ef1a6
JKukielski <jankukielski1@gmail.com>
2024-03-13 17:59:40 +0100

    Removing placeholder data fetching

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
 import styles from './singlePost.module.css';
 import PostUser from '@/components/postUser/postUser';
+import { Suspense } from 'react';
+import { getPost } from '@/lib/data';
 
-const getData = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+// const getData = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-  if (!res.ok) {
-    throw new Error('Something went wrong.');
-  }
+//   if (!res.ok) {
+//     throw new Error('Something went wrong.');
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const SinglePostPage = async ({ params }) => {
   const { slug } = params;
 
-  const post = await getData(slug);
+  const post = await getPost(slug);
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -27,7 +29,7 @@ const SinglePostPage = async ({ params }) => {
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
+        <h1 className={styles.title}>{post?.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -36,13 +38,17 @@ const SinglePostPage = async ({ params }) => {
             height={50}
             alt=""
           />
-          <PostUser userId={post.userId} />
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post?.body}</div>
       </div>
     </div>
   );
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,19 +1,21 @@
+import { getUser } from '@/lib/data';
 import styles from './postUser.module.css';
 
-const getData = async (userId) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
-  );
+// const getData = async (userId) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/users/${userId}`,
+//     { cache: 'no-store' }
+//   );
 
-  if (!res.ok) {
-    throw new Error('Something went wrong.');
-  }
+//   if (!res.ok) {
+//     throw new Error('Something went wrong.');
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const PostUser = async ({ userId }) => {
-  const user = await getData(userId);
+  const user = await getUser(userId);
   return (
     <div className={styles.container}>
       <span className={styles.title}>Author</span>
