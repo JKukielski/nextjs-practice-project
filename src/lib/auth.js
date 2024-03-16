d2660685280fba139b936b3001e7159969e399e4
JKukielski <jankukielski1@gmail.com>
2024-03-16 13:54:50 +0100

    Added CredentialsProvider

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,35 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { connectToDb } from './utils';
 import { User } from './models';
+import bcrypt from 'bcryptjs';
+
+const login = async (credentials) => {
+  try {
+    connectToDb();
+    const user = await User.findOne({ username: credentials.username });
+
+    if (!user) {
+      throw new Error('Incorrect credentials');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      throw new Error('Incorrect credentials');
+    }
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to login');
+  }
+};
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,6 +40,16 @@ export const {
     GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          return user;
+        } catch (error) {
+          return null;
+        }
+      },
     }),
   ],
   callbacks: {
