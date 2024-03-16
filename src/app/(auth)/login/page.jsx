8a8572c6942cbfde3336bc2586d9815dee07d13e
JKukielski <jankukielski1@gmail.com>
2024-03-16 13:55:48 +0100

    Login form

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,15 @@
-import { handleGithubLogin } from '@/lib/actions';
+import { handleGithubLogin, login } from '@/lib/actions';
 
 const LoginPage = async () => {
   return (
     <div>
       <form action={handleGithubLogin}>
         <button>Login with Github</button>
+      </form>
+      <form action={login}>
+        <input type="text" placeholder="Username" name="username" />
+        <input type="text" placeholder="Password" name="password" />
+        <button>Login with Credentials</button>
       </form>
     </div>
   );
