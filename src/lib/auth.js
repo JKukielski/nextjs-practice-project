3bc3941cb8f8ea35c00de8e1858358727fbe30bb
JKukielski <jankukielski1@gmail.com>
2024-03-16 13:25:54 +0100

    Callback for adding github user to database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { connectToDb } from './utils';
+import { User } from './models';
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,4 +14,26 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === 'github') {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              img: profile.avatar_url,
+            });
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
