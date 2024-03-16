2a52915470606c5f9aa36fa860180a81a0622bdf
JKukielski <jankukielski1@gmail.com>
2024-03-16 13:55:06 +0100

    Login and register actions

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,9 +1,10 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
-import { Post } from './models';
+import { Post, User } from './models';
 import { connectToDb } from './utils';
 import { signIn, signOut } from './auth';
+import bcrypt from 'bcryptjs';
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -44,4 +45,50 @@ export const handleGithubLogin = async (e) => {
 export const handleGithubLogout = async (e) => {
   'use server';
   await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, email, password, img, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return 'Passwords do not match';
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return 'Username already exists';
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+
+    await newUser.save();
+    console.log('Saved to the database');
+  } catch (error) {
+    console.log(error);
+    return { error: 'Something went wrong' };
+  }
+};
+
+export const login = async (formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn('credentials', { username, password });
+  } catch (error) {
+    console.log(error);
+    return { error: 'Something went wrong' };
+  }
 };
